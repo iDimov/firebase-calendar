b06333b2a0d909ff03581c128584417d3c65ac29
iDimov <iDimov@users.noreply.github.com>
2022-09-16 11:04:00 +0300

    setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'calendar-f9e41',
+    appId: '1:264634693788:web:08666546deaeb28b375559',
+    databaseURL: 'https://calendar-f9e41-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'calendar-f9e41.appspot.com',
+    apiKey: 'AIzaSyDtkrN0z4koJyboBQXB3wOuRx9HOc9GE38',
+    authDomain: 'calendar-f9e41.firebaseapp.com',
+    messagingSenderId: '264634693788',
+  },
   production: true
 };
